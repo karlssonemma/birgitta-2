6854bd83e66bb6207a673a086ca497afdae53d6c
emma <karlssonemma93@gmail.com>
2022-09-06 17:42:05 +0200

    deleted product folder in components since it was empty. also deleted default import of that folder in index.js.

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ export * from './cards/index';
 export * from './cart/index';
 export * from './elements/index';
 export * from './global/index';
-export * from './product/index';
 export * from './sections/index';
 export {CountrySelector} from './CountrySelector.client';
 export {HeaderFallback} from './HeaderFallback';

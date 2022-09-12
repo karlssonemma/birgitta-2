99534cd7e29bd3906330b73b319ab55920fa24d1
emma <karlssonemma93@gmail.com>
2022-09-12 12:43:37 +0200

    deleted export of /product in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,5 @@ export * from './cart/index';
 export * from './elements/index';
 export * from './global/index';
 export * from './sections/index';
-export * from './product/index';
 export {CountrySelector} from './CountrySelector.client';
 export {HeaderFallback} from './HeaderFallback';

014a89b67f539b34425941799b3abee78db2864f
emma <karlssonemma93@gmail.com>
2022-09-19 13:20:26 +0200

    deleted unused code home

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -12,11 +12,7 @@ import {
   Link
 
 } from '@shopify/hydrogen';
-
-import {MEDIA_FRAGMENT, PRODUCT_CARD_FRAGMENT} from '~/lib/fragments';
-import {getHeroPlaceholder} from '~/lib/placeholders';
-import {FeaturedCollections, Hero} from '~/components';
-import {Layout, ProductSwimlane} from '~/components/index.server';
+import {Layout} from '~/components/index.server';
 
 import ArrowIcon from '../components/ArrowIcon';
 import FeaturedSection from '../components/FeaturedSection.client';
@@ -72,7 +68,7 @@ export default function Homepage() {
             title: "Home"
           }} 
         />
-        <div className='h-max md:h-[calc(100vh-8rem)] text-blue-200 flex flex-col-reverse md:flex-row lg:justify-start items-center gap-24'>
+        <div className='h-max md:h-[calc(100vh-8rem)] text-black flex flex-col-reverse md:flex-row lg:justify-start items-center gap-24'>
           <Image 
             data={page.image.reference.image}
             width={2000}
@@ -100,89 +96,30 @@ const LinkToShop = () => {
   )
 }
 
-// function SeoForHomepage() {
-//   const {
-//     data: {
-//       shop: {title, description},
-//     },
-//   } = useShopQuery({
-//     query: HOMEPAGE_SEO_QUERY,
-//     cache: CacheLong(),
-//     preload: true,
-//   });
+function SeoForHomepage() {
+  const {
+    data: {
+      shop: {title, description},
+    },
+  } = useShopQuery({
+    query: HOMEPAGE_SEO_QUERY,
+    cache: CacheLong(),
+    preload: true,
+  });
 
-//   return (
-//     <Seo
-//       type="homepage"
-//       data={{
-//         title,
-//         description,
-//         titleTemplate: '%s · Powered by Hydrogen',
-//       }}
-//     />
-//   );
-// }
 
-// const HOMEPAGE_CONTENT_QUERY = gql`
-//   ${MEDIA_FRAGMENT}
-//   ${PRODUCT_CARD_FRAGMENT}
-//   query homepage($country: CountryCode, $language: LanguageCode)
-//   @inContext(country: $country, language: $language) {
-//     heroBanners: collections(
-//       first: 3
-//       query: "collection_type:custom"
-//       sortKey: UPDATED_AT
-//     ) {
-//       nodes {
-//         id
-//         handle
-//         title
-//         descriptionHtml
-//         heading: metafield(namespace: "hero", key: "title") {
-//           value
-//         }
-//         byline: metafield(namespace: "hero", key: "byline") {
-//           value
-//         }
-//         cta: metafield(namespace: "hero", key: "cta") {
-//           value
-//         }
-//         spread: metafield(namespace: "hero", key: "spread") {
-//           reference {
-//             ...Media
-//           }
-//         }
-//         spreadSecondary: metafield(namespace: "hero", key: "spread_secondary") {
-//           reference {
-//             ...Media
-//           }
-//         }
-//       }
-//     }
-//     featuredCollections: collections(
-//       first: 3
-//       query: "collection_type:smart"
-//       sortKey: UPDATED_AT
-//     ) {
-//       nodes {
-//         id
-//         title
-//         handle
-//         image {
-//           altText
-//           width
-//           height
-//           url
-//         }
-//       }
-//     }
-//     featuredProducts: products(first: 12) {
-//       nodes {
-//         ...ProductCard
-//       }
-//     }
-//   }
-// `;
+  return (
+    <Seo
+      type="homepage"
+      data={{
+        title,
+        description,
+        titleTemplate: '%s · Home',
+      }}
+    />
+  );
+}
+
 
 const QUERY = gql`
 query PageDetails(
